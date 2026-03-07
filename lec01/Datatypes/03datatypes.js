9c4ccbd995a4c7ed185fd204f56a877c9b07908f
Sagardeep Choudhury <sagardeepchoudhury01@gmail.com>
2026-03-07 22:46:36 +0530

    03datatypes.js implemented

---
+++ lec01/Datatypes/03datatypes.js
@@ -0,0 +1,16 @@
+// why NaN is a number type
+
+console.log(typeof NaN); // type is number as : NaN is a failed operation of number
+
+let a;
+console.log(a); // in JS bydefault after declatation is undefined before initialisation
+
+let b = null; // mannually implemented
+console.log(b);
+
+console.log("5"+1);
+console.log(9+"4");
+
+console.log(7-"9");
+
+
